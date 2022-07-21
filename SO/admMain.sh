7b63ccbd27a2a7c207a319b71d087289a294493b
Bruno <carrizo729@gmail.com>
2022-07-21 18:16:16 -0300

    Nuevas funciones agruegadas. Posibilidad de modificar nombre y grupo primario del usuario.

--- SO/admMain.sh
+++ SO/admMain.sh
@@ -106,32 +106,115 @@ function agruegarUsuario() {
   fi
 }
 
+function borrarUsuario(){
+  clear
+  read -p "Ingrese nombre de usuario a eliminar: " usuario
+  consulta=$(cat /etc/passwd | grep "$usuario" | cut -d: -f1)
+
+  if [[ "$usuario" == "$consulta" ]]; then
+    sudo userdel -r -f "$consulta" 2> .temp.txt
+    echo "Usuario eliminado correctamente"
+    rm .temp.txt
+  else
+    echo "El usuario no existe"
+  fi
+}
+
+function modificarUsuario(){
+  clear
+  #Pido el nombre actual del usuario
+  clear
+  read -p "Ingrese nombre de usuario a modificar: " usuario
+  consulta=$(cat /etc/passwd | grep "$usuario" | cut -d: -f1)
+
+  if [[ "$usuario" == "$consulta" ]]; then
+    clear
+  else
+    echo "Usuario no encontrado"
+    sleep 1
+    clear
+    return
+  fi
+
+  #Menú de opciones para modificar
+  select opcion in "Cambiar nombre" "Cambiar grupo primario" "Cambiar contraseña" "Salir"
+  do
+    case $REPLY in
+      1)
+      #Ingreso del nuevo nombre
+      validacionNombre=$(validarNombre "Ingrese el nuevo nombre para el usuario: ")
+      validacion=$(echo "$validacionNombre" | grep "[0-9]")
+      nombre=$(echo "$validacionNombre" | grep "^[a-z?A-Z?]*$")
+
+      if [[ "$validacion" == "0" ]]; then
+        clear
+      else
+        while [[ "$validacion" != "0" ]]; do
+          clear
+          echo -e "¡Ingresó un nombre invalido!"
+          validacionNombre=$(validarNombre "Ingrese un nombre para el nuevo usuario: ")
+          validacion=$(echo "$validacionNombre" | grep "[0-9]")
+          nombre=$(echo "$validacionNombre" | grep "^[a-z?A-Z?]*$")
+        done
+      fi
+
+      sudo usermod -l "$nombre" "$usuario"
+
+      break
+      ;;
+      2)
+      read -p "Ingrese el nombre del nuevo grupo primario: " grpPrimario
+      consulta=$(cat /etc/group | cut -d: -f1 | grep ^"$grpPrimario"$)
+
+      if [[ "$grpPrimario" == "$consulta" ]]; then
+        sudo usermod -g "$consulta" "$usuario"
+        echo "Usuario cambiado de grupo exitosamente."
+        sleep 3
+        clear
+      else
+        echo "El grupo no existe."
+        sleep 2
+        clear
+      fi
+      break
+      ;;
+      3)
+
+      break
+      ;;
+      4)
+      clear
+      break
+      ;;
+      *)"Opción incorrecta"
+      break
+      ;;
+    esac
+  done
+}
+
+
+
 ############ Menú principal ############
 while [[ true ]]; do
+  echo -e "Menú de administración de usuarios\n"
     select opcion in "Agruegar usuario" "Eliminar usuario" "Modificar usuario" "Salir"
   do
     case $REPLY in
       1)
       agruegarUsuario
-
       break
       ;;
-      2)clear
-      read -p "Ingrese nombre de usuario a eliminar: " usuario
-      consulta=$(cat /etc/passwd | grep "$usuario" | cut -d: -f1)
-
-      if [[ "$usuario" == "$consulta" ]]; then
-        sudo userdel -r -f "$consulta" 2> .temp.txt
-        rm .temp.txt
-      else
-        echo "El usuario no existe"
-      fi
+      2)
+      borrarUsuario
       break
       ;;
-      3)clear
+      3)
+      modificarUsuario
       break
       ;;
-      4)exit
+      4)clear
+      exit
       ;;
       *)"Opción incorrecta"
       break
