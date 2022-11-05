dc58572c9db1bddf5cd6054837a0ef259ebb7c16
Bruno <carrizo729@gmail.com>
2022-11-05 05:45:19 -0300

    Script de respaldos mejorado

--- SO/Scripts/respaldos.sh
+++ SO/Scripts/respaldos.sh
@@ -11,8 +11,12 @@ while [[ true ]]; do
   do
     case $REPLY in
       1)clear
-      rsync -aAXv --exclude={/dev/*,/usr/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/backup/*} /* /backup/respaldo-$(date +%Y-%m-%d-%H.%M.%S)/
+      fecha=$(date +%Y-%m-%d-%H.%M.%S)
 
+      mysqldump -u root -prtBd1234. kefruta > /backupbd/respaldo-"$fecha".sql
+      tar -cvf /var/log/mariadb/* /backupbd/respaldo-binario-"$fecha"
+      rsync -aAXv --exclude={/dev/*,/usr/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/backup/*} /* /backup/respaldo-"$fecha"/
+      mv /backupbd/respaldo-"$fecha".sql /backupbd/respaldo-binario-"$fecha" /backup/respaldo-"$fecha"/* /media/respaldosKefruta
       clear
       break
       ;;
