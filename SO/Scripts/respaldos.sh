cd0bc8279b7c35b8bd39626838f796b6a71c69fb
Bruno <carrizo729@gmail.com>
2022-11-05 06:22:10 -0300

    Se agruego respaldo de base de datos de forma local.

--- SO/Scripts/respaldos.sh
+++ SO/Scripts/respaldos.sh
@@ -7,23 +7,24 @@ PS3="Ingrese una opción: "
 
 while [[ true ]]; do
   echo -e "Menú de respaldos\n"
-    select opcion in "Respaldo completo local" "Respaldo completo servidor respaldo" "Volver"
+    select opcion in "Respaldo completo del sistema local" "Respaldo completo de base de datos" "Volver"
   do
     case $REPLY in
       1)clear
       fecha=$(date +%Y-%m-%d-%H.%M.%S)
-
       mysqldump -u root -prtBd1234. kefruta > /backupbd/respaldo-"$fecha".sql
-      read -p "asd" NaN
       tar -cvf /backupbd/respaldo-binario-"$fecha".tar /var/log/mariadb/*
-      read -p "asd" NaN
       rsync -aAXv --exclude={/dev/*,/usr/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/backup/*,/root/*} /* /backup/respaldo-"$fecha"/
-      #mkdir /media/respaldosKefruta/respaldo-"$fecha"/
       mv /backup/respaldo-"$fecha"/ /media/respaldosKefruta/
       clear
       break
       ;;
-      2)
+      2)clear
+      fecha=$(date +%Y-%m-%d-%H.%M.%S)
+      mysqldump -u root -prtBd1234. kefruta > /backupbd/respaldo-"$fecha".sql
+      tar -cvf /backupbd/respaldo-binario-"$fecha".tar /var/log/mariadb/*
+      ;;
+      3)
       exit
       ;;
       *)clear
