f672e3b38f9c2fa939f47524ffabcaa3e9b574aa
Bruno <carrizo729@gmail.com>
2022-11-05 09:31:25 -0300

    BUG FIX

--- SO/Scripts/borkenbdscript.sh
+++ SO/Scripts/borkenbdscript.sh
@@ -146,8 +146,18 @@ function borrarUsuario(){
     done
   fi
 
-  #mysql -u consultaUsuarios --password=consultaBd1234. < Sql/selectUser.sql > Sql/.resultado.txt
-  mysql -u root --password=rtBd1234. < Sql/selectUser.sql > Sql/.resultado.txt
+  #mysql -u consultaUsuarios -pconsultaBd1234. < Sql/selectUser.sql > Sql/.resultado.txt
+mysql -u root -pAa5254598* < Sql/selectUser.sql > Sql/.resultado.txt
+  existe=$(cat Sql/.resultado.txt | grep -o "^$nombre[[:blank:]]\+" | grep "^$nombre[[:blank:]]\+$")
+
+  echo "$nombre"
+  echo "$existe"
+  read -p "asd" asd
+  if [[ "$existe" != "$nombre" ]]; then
+    echo "¡El usuario no existe!"
+    sleep 2
+  else
+
   multiCuenta=$(cat Sql/.resultado.txt | grep "^$nombre[[:blank:]]\+" | grep -n "" | grep -o "2")
 
   if [[ "$multiCuenta" == "2"  ]]; then
@@ -200,11 +210,12 @@ function borrarUsuario(){
     rm Sql/.resultado.txt
     sleep 1
     clear
+  fi
 }
 #################################################################################
 function listarUsuario(){
   clear
-  echo -e "Toque enter para volver\n\n"
+  echo -e "Toque cualquier tecla para volver\n\n"
   mysql -u root --password=rtBd1234. -e "SELECT User,Host FROM mysql.user"
   #mysql -u consultaUsuarios < Sql/selectUser.sql > Sql/.resultado.txt
   #cat Sql/.resultado.txt
@@ -212,10 +223,23 @@ function listarUsuario(){
   #rm Sql/.resultado.txt
   clear
 }
+#################################################################################
+function subirMotor(){
+  clear
+  (systemctl start mariadb && echo "Motor iniciado correctamente") || echo "Error al inicar el motor de base de datos"
+  sleep 3
+}
+#################################################################################
+function bajarMotor(){
+  clear
+  (systemctl stop mariadb && echo "Motor parado correctamente") || echo "Error al parar el motor de base de datos"
+  sleep 3
+}
+
 ############ Menú principal ############
 while [[ true ]]; do
   echo -e "Menú de adminitración de base de datos\n"
-    select opcion in "Agruegar un administrador de BD" "Borrar usuario" "Listar usuarios actuales" "Volver"
+    select opcion in "Agruegar un administrador de BD" "Borrar usuario" "Listar usuarios actuales" "Iniciar motor de base de datos" "Parar motor de base de datos" "Volver"
   do
     case $REPLY in
       1)
@@ -234,6 +258,16 @@ while [[ true ]]; do
       break
       ;;
       4)
+      subirMotor
+      clear
+      break
+      ;;
+      5)
+      bajarMotor
+      clear
+      break
+      ;;
+      6)
       exit
       clear
       ;;
