1064a57ce3775fe3f2cba19c1a37ab6ac542ba88
Bruno <carrizo729@gmail.com>
2022-11-05 09:46:06 -0300

    BUG FIX?

--- SO/Scripts/testRed.sh
+++ SO/Scripts/testRed.sh
@@ -52,7 +52,7 @@ while [[ true ]]; do
     case $REPLY in
       1)clear
       ping -c 4 google.com && echo -e "\nHay conexión a internet" || echo -e "\nNo hay conexión a internet"
-
+ 
       echo ""
 
       read -p "Ingrese ENTER para volver" noVal
