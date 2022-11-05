cf7b0dd50f447ecfba2e16ae1d2c9655e4d2b773
Bruno <carrizo729@gmail.com>
2022-11-05 06:32:20 -0300

    Prueba

--- SO/Scripts/respaldos.sh
+++ SO/Scripts/respaldos.sh
@@ -21,8 +21,8 @@ while [[ true ]]; do
       ;;
       2)clear
       fecha=$(date +%Y-%m-%d-%H.%M.%S)
-      mysqldump -u root -prtBd1234. kefruta > /backupbd/respaldo-"$fecha".sql
-      tar -cvf /backupbd/respaldo-binario-"$fecha".tar /var/log/mariadb/*
+      (mysqldump -u root -prtBd1234. kefruta > /backupbd/respaldo-"$fecha".sql && tar -cvf /backupbd/respaldo-binario-"$fecha".tar /var/log/mariadb/* && echo "Backup relizadó con exito") || echo "No se pudo hacer el respaldo, contacte con el adminitrador del sistema"
+
       clear
       break
       ;;
